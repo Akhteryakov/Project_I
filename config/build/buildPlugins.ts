2d25bfac31314359bea1ab3b60f75f9432fd114b
Akhteryakov <aht-alex@mail.ru>
2023-09-26 11:20:34 +0400

    feat: add HotModuleReplacementPlugin for Webpack

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -3,6 +3,7 @@ import webpack from "webpack";
 import HTMLWebpackPlugin from "html-webpack-plugin";
 import { BuildOptions } from "./types/config";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
 
 export function buildPlugins({
   paths,
@@ -20,5 +21,6 @@ export function buildPlugins({
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
     }),
-  ];
+    isDev && new webpack.HotModuleReplacementPlugin(),
+  ].filter(Boolean);
 }
