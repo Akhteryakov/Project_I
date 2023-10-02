d3041c29d1955b03bcbf788c25022dda4e84bf81
Akhteryakov <aht-alex@mail.ru>
2023-10-02 14:39:22 +0400

    feat: add Loader and PageLoader components

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,12 +1,13 @@
 import { Suspense } from 'react';
 import { useRoutes, RouteObject } from 'react-router-dom';
+import { PageLoader } from 'widget/PageLoader';
 import { routeConfig } from './routeConfig';
 
 const AppRouter = () => {
   const elements = useRoutes(Object.values(routeConfig) as RouteObject[]);
 
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<PageLoader />}>
       <div className="page-wrapper">{elements}</div>
     </Suspense>
   );
