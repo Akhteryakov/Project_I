30a842754a1b1f23c8897852991a7779d214da19
Akhteryakov <aht-alex@mail.ru>
2023-10-02 14:09:21 +0400

    feat: add NotFoundPage and rout for it

--- src/app/providers/router/ui/routeConfig.tsx
+++ src/app/providers/router/ui/routeConfig.tsx
@@ -1,7 +1,8 @@
-import { AboutPage } from 'pages/AboutPage';
-import { MainPage } from 'pages/MainPage';
-import { RouteProps } from 'react-router-dom';
-import { AppRoutes, RoutePath } from 'shared/routes';
+import { AboutPage } from "pages/AboutPage";
+import { MainPage } from "pages/MainPage";
+import { NotFoundPage } from "pages/NotFoundPage";
+import { RouteProps } from "react-router-dom";
+import { AppRoutes, RoutePath } from "shared/routes";
 
 export const routeConfig: Record<AppRoutes, RouteProps> = {
   [AppRoutes.MAIN]: {
@@ -11,5 +12,9 @@ export const routeConfig: Record<AppRoutes, RouteProps> = {
   [AppRoutes.ABOUT]: {
     path: RoutePath[AppRoutes.ABOUT],
     element: <AboutPage />,
+  },
+  [AppRoutes.NOT_FOUND]: {
+    path: RoutePath[AppRoutes.NOT_FOUND],
+    element: <NotFoundPage />,
   },
 };
--- src/shared/routes/appRoutes/appRoutes.ts
+++ src/shared/routes/appRoutes/appRoutes.ts
@@ -1,13 +1,15 @@
-import { AboutPage } from 'pages/AboutPage';
-import { MainPage } from 'pages/MainPage';
-import { RouteProps } from 'react-router-dom';
+import { AboutPage } from "pages/AboutPage";
+import { MainPage } from "pages/MainPage";
+import { RouteProps } from "react-router-dom";
 
 export enum AppRoutes {
-  MAIN = 'main',
-  ABOUT = 'about',
+  MAIN = "main",
+  ABOUT = "about",
+  NOT_FOUND = "not_found",
 }
 
 export const RoutePath: Record<AppRoutes, string> = {
-  [AppRoutes.MAIN]: '/',
-  [AppRoutes.ABOUT]: '/about',
+  [AppRoutes.MAIN]: "/",
+  [AppRoutes.ABOUT]: "/about",
+  [AppRoutes.NOT_FOUND]: "*",
 };
