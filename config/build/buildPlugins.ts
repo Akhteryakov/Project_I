56da3346be24f5d2860285adb785ea6d8d215265
Akhteryakov <aht-alex@mail.ru>
2023-10-09 15:20:50 +0400

    fix checks

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -8,7 +8,7 @@ export function buildPlugins({
   paths,
   isDev,
 }: BuildOptions): webpack.WebpackPluginInstance[] {
-  return [
+  const plugins = [
     new webpack.ProgressPlugin(),
     new HTMLWebpackPlugin({
       template: paths.html,
@@ -19,10 +19,15 @@ export function buildPlugins({
     }),
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
-    }),
-    isDev && new webpack.HotModuleReplacementPlugin(),
-    new BundleAnalyzerPlugin(
-      { analyzerMode: process.env.STATS as 'server' || 'disabled' },
-    )
-  ].filter(Boolean);
+    })    
+  ]
+
+  if (isDev) {
+    plugins.push(new webpack.HotModuleReplacementPlugin())
+    plugins.push(new BundleAnalyzerPlugin(
+      { openAnalyzer: false },
+    ))
+  }
+  
+  return plugins;
 }
